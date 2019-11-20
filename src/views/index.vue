b6368ef1edf0972631be66ea6d020d1f0be22898
chenguoqing <chenguoqing@zhuojianchina.com>
2019-11-20 19:30:41 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
         这是{{page}}页面
+        <button @click="reverse">按钮</button>
+        <div>{{message}}</div>
+        <counter></counter>
     </div>
 </template>
 <script>
+    // import Vue from "vue"
+    // Vue.component('counter',{
+    //     data() {
+    //         return {
+    //             count: 0
+    //         }
+    //     },
+    //     template: '<span>{{count}}</span>'
+    // })
 export default {
     data: function () {
         return {
-            page: '首页'
+            page: '首页',
+            message: 'hello vue!'
+        }
+    },
+    methods:{
+        reverse(){
+            this.message = this.message.split('').reverse().join('-')
         }
     }
 }
